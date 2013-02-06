cbec57ea99b32be90076c701f4b02c39fb6085f3
kadirpekel <kadirpekel@gmail.com>
2013-02-06 08:52:52 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='octavious',
+    py_modules=['octavious'],
+    version='0.0.1',
+    description='lightweight, modular parallelizing framework',
+    author='Metglobal',
+    author_email='kadir.pekel@metglobal.com',
+    url='https://github.com/metglobal/octavious',
+)
