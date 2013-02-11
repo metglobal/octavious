70b4422f08364b6417ffbab68b0b4ab866d04b2c
kadirpekel <kadirpekel@gmail.com>
2013-02-11 16:30:11 +0200

    fixed chucknjoke process

--- examples/simple/processes/chucknjoke.py
+++ examples/simple/processes/chucknjoke.py
@@ -6,6 +6,6 @@ from octavious.processor import Processor
 class ChuckNJokeProcessor(Processor):
 
     def process(self, input):
-        return urllib.urlopen('http://api.icndb.com/jokes/random').read()
+        return urllib.urlopen('http://api.icndb.com/jokes/%s' % input).read()
 
 processor = ChuckNJokeProcessor
