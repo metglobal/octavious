edceefdd8b68812aa80ebbd254a97dfd8eca0598
kadirpekel <kadirpekel@gmail.com>
2013-03-11 13:08:32 +0200

    added a primitive celery driver, added parallelizer switcher to simple example

--- examples/simple/__main__.py
+++ examples/simple/__main__.py
@@ -1,20 +1,24 @@
+import sys
+
 from octavious.utils import pipeline, processor, plugin, parallelizer
 from octavious.processor import ManyToOneProcessor, PipelineProcessor
 
 
 if __name__ == '__main__':
 
+    driver = len(sys.argv) > 1 and sys.argv[1] or 'multiprocessing'
+
     cnj_processor = processor('examples.simple.processes.chucknjoke')
 
     cnj_pipeline = pipeline(
-        plugin('examples.simple.plugins.echo'),
         plugin('examples.simple.plugins.dictdigger', 'value.joke'),
         plugin('examples.simple.plugins.jsondeserializer'))
 
-    cnj_parallelizer = parallelizer('octavious.parallelizer.multiprocessing')
+    cnj_parallelizer = parallelizer('octavious.parallelizer.%s' % driver)
 
-    onetomany = ManyToOneProcessor(PipelineProcessor(cnj_processor,
+    manytoone = ManyToOneProcessor(PipelineProcessor(cnj_processor,
                                                      cnj_pipeline),
                                    cnj_parallelizer)
 
-    onetomany(range(1, 4))
+    for result in manytoone(range(1, 4)):
+        print '*', result
